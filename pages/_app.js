adcc0fe2db62dbf2114096f1509a9f2f0767be73
Mac <mac@Usmans-Mac.local>
2024-01-22 12:43:09 +0500

    mergin resolve for all screen

--- pages/_app.js
+++ pages/_app.js
@@ -4,10 +4,32 @@ import Footer from "../components/Footer/Footer";
 
 export default function App({ Component, pageProps }) {
   return (
-    <>
+    <div>
       <Navbar />
       <Component {...pageProps} />
       <Footer />
-    </>
+      <style jsx global>{`
+        html,
+        body {
+          padding: 0;
+          margin: 0;
+          font-family:
+            -apple-system,
+            BlinkMacSystemFont,
+            Segoe UI,
+            Roboto,
+            Oxygen,
+            Ubuntu,
+            Cantarell,
+            Fira Sans,
+            Droid Sans,
+            Helvetica Neue,
+            sans-serif;
+        }
+        * {
+          box-sizing: border-box;
+        }
+      `}</style>
+    </div>
   );
 }
