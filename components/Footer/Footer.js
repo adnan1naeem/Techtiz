e9b7fcc38655f77ffd5ada298a5b849ed94ccb72
nomanrauf02 <nomanrauf02@gmail.com>
2024-01-22 18:49:30 +0500

    footer links activation

--- components/Footer/Footer.js
+++ components/Footer/Footer.js
@@ -8,65 +8,80 @@ import { FaFacebookF } from "react-icons/fa";
 import { AiFillInstagram } from "react-icons/ai";
 
 function Footer() {
-    return (
-        <Box sx={{ background: "#F8F8F8", paddingTop: "3%", marginTop: "4%" }}>
-            <Box
-                sx={{
-                    display: "flex",
-                    gap: "2%",
-                    justifyContent: "center",
-                    flexWrap: "wrap",
-                    paddingBottom: "5%",
-                }}
-            >
-                <FooterLogoCard />
-                <Box sx={{ display: "flex", flexWrap: "wrap" }}>
-                    <FooterCard
-                        title="Company"
-                        text1="About"
-                        text2="Industry"
-                        text3="Case Study"
-                        text4="Contact Us"
-                        text5="Privacy Policy"
-                    />
-                    <FooterCard
-                        title="Services"
-                        text1="Mobile App Development"
-                        text2="Web Development"
-                        text3="UI/UX Design"
-                        text4="SQA"
-                    />
-                    <FooterCard title="Resources" text1="Careers" text2="Blog" />
-                    <FooterReach />
-                </Box>
-            </Box>
-            <Box
-                sx={{
-                    marginTop: "2%",
-                    borderTop: "1px solid #ccc",
-                    display: "flex",
-                    justifyContent: "space-between",
-                    flexWrap: "wrap",
-                    paddingLeft: "10%",
-                    paddingRight: "10%",
-                    paddingTop: "5%",
-                    paddingBottom: "2.5%",
-                }}
-            >
-                <Box>
-                    <Typography sx={{ color: "#153A5F", fontWeight: "100" }}>
-                        © 2023 Techtiz. All Rights Reserved.
-                    </Typography>
-                </Box>
-                <Box sx={{ display: "flex", gap: "1rem" }}>
-                    {" "}
-                    <FaLinkedinIn style={{ fontSize: "28px", color: "#153A5F" }} />
-                    <FaFacebookF style={{ fontSize: "24px", color: "#153A5F" }} />
-                    <AiFillInstagram style={{ fontSize: "28px", color: "#153A5F" }} />
-                </Box>
-            </Box>
+  return (
+    <Box sx={{ background: "#F8F8F8", paddingTop: "3%", marginTop: "4%" }}>
+      <Box
+        sx={{
+          display: "flex",
+          gap: "2%",
+          justifyContent: "center",
+          flexWrap: "wrap",
+          paddingBottom: "5%",
+        }}
+      >
+        <FooterLogoCard />
+        <Box sx={{ display: "flex", flexWrap: "wrap" }}>
+          <FooterCard
+            title="Company"
+            text1="About"
+            href_one="/about"
+            text2="Industry"
+            href_two="/industries"
+            text3="Case Study"
+            href_three="/portfolios"
+            text4="Contact Us"
+            href_four="/get-started"
+            text5="Privacy Policy"
+            href_five="/"
+          />
+          <FooterCard
+            title="Services"
+            text1="Mobile App Development"
+            href_one="/mobile-application"
+            text2="Web Development"
+            href_two="/website"
+            text3="UI/UX Design"
+            href_three="/ui-ux-design"
+            text4="SQA"
+            href_four="/sqa"
+          />
+          <FooterCard
+            title="Resources"
+            text1="Careers"
+            href_one="/career"
+            text2="Blog"
+            href_two="/blogs"
+          />
+          <FooterReach />
         </Box>
-    );
+      </Box>
+      <Box
+        sx={{
+          marginTop: "2%",
+          borderTop: "1px solid #ccc",
+          display: "flex",
+          justifyContent: "space-between",
+          flexWrap: "wrap",
+          paddingLeft: "10%",
+          paddingRight: "10%",
+          paddingTop: "5%",
+          paddingBottom: "2.5%",
+        }}
+      >
+        <Box>
+          <Typography sx={{ color: "#153A5F", fontWeight: "100" }}>
+            © 2023 Techtiz. All Rights Reserved.
+          </Typography>
+        </Box>
+        <Box sx={{ display: "flex", gap: "1rem" }}>
+          {" "}
+          <FaLinkedinIn style={{ fontSize: "28px", color: "#153A5F" }} />
+          <FaFacebookF style={{ fontSize: "24px", color: "#153A5F" }} />
+          <AiFillInstagram style={{ fontSize: "28px", color: "#153A5F" }} />
+        </Box>
+      </Box>
+    </Box>
+  );
 }
 
 export default Footer;
--- components/FooterCard/FooterCard.js
+++ components/FooterCard/FooterCard.js
@@ -1,95 +1,139 @@
 import { Box, Card, CardContent, Typography } from "@mui/material";
 import React from "react";
-function FooterCard({ title, text1, text2, text3, text4, text5 }) {
-    return (
-        <Box>
-            <Card
-                sx={{
-                    height: { sx: "17rem", sm: "21rem", md: "19rem", lg: "17rem" },
-                    width: { sx: "10rem", sm: "7rem", md: "9rem", lg: "12rem" },
-                    background: "none",
-                    boxShadow: "none",
-                }}
+import Link from "next/link";
+
+function FooterCard({
+  title,
+  text1,
+  text2,
+  text3,
+  text4,
+  text5,
+  href_one,
+  href_two,
+  href_three,
+  href_four,
+  href_five,
+}) {
+  return (
+    <Box>
+      <Card
+        sx={{
+          height: { sx: "17rem", sm: "21rem", md: "19rem", lg: "17rem" },
+          width: { sx: "10rem", sm: "7rem", md: "9rem", lg: "12rem" },
+          background: "none",
+          boxShadow: "none",
+        }}
+      >
+        <CardContent sx={{ display: "flex", flexDirection: "column" }}>
+          <Typography
+            sx={{
+              color: "#153A5F",
+              fontSize: "18px",
+              fontWeight: "600",
+              paddingBottom: "12%",
+            }}
+          >
+            {title}
+          </Typography>
+
+          <Link
+            style={{ textDecoration: "none" }}
+            href={{ pathname: href_one }}
+          >
+            <Typography
+              sx={{
+                color: "#153A5F",
+                "&:hover": {
+                  color: "#18B0E6",
+                },
+                cursor: "pointer",
+                fontSize: "17px",
+              }}
             >
-                <CardContent sx={{ display: "flex", flexDirection: "column" }}>
-                    <Typography
-                        sx={{
-                            color: "#153A5F",
-                            fontSize: "18px",
-                            fontWeight: "600",
-                            paddingBottom: "12%",
-                        }}
-                    >
-                        {title}
-                    </Typography>
-                    <Typography
-                        sx={{
-                            color: "#153A5F",
-                            "&:hover": {
-                                color: "#18B0E6",
-                            },
-                            cursor: "pointer",
-                            fontSize: "17px",
-                        }}
-                    >
-                        {text1}
-                    </Typography>
-                    <Typography
-                        sx={{
-                            color: "#153A5F",
-                            marginTop: "10px",
-                            "&:hover": {
-                                color: "#18B0E6",
-                            },
-                            cursor: "pointer",
-                            fontSize: "17px",
-                        }}
-                    >
-                        {text2}
-                    </Typography>
-                    <Typography
-                        sx={{
-                            color: "#153A5F",
-                            marginTop: "10px",
-                            "&:hover": {
-                                color: "#18B0E6",
-                            },
-                            cursor: "pointer",
-                            fontSize: "17px",
-                        }}
-                    >
-                        {text3}
-                    </Typography>
-                    <Typography
-                        sx={{
-                            color: "#153A5F",
-                            marginTop: "10px",
-                            "&:hover": {
-                                color: "#18B0E6",
-                            },
-                            cursor: "pointer",
-                            fontSize: "17px",
-                        }}
-                    >
-                        {text4}
-                    </Typography>
-                    <Typography
-                        sx={{
-                            color: "#153A5F",
-                            marginTop: "10px",
-                            "&:hover": {
-                                color: "#18B0E6",
-                            },
-                            cursor: "pointer",
-                            fontSize: "17px",
-                        }}
-                    >
-                        {text5}
-                    </Typography>
-                </CardContent>
-            </Card>
-        </Box>
-    );
+              {text1}
+            </Typography>
+          </Link>
+
+          <Link
+            style={{ textDecoration: "none" }}
+            href={{ pathname: href_two }}
+          >
+            <Typography
+              sx={{
+                color: "#153A5F",
+                marginTop: "10px",
+                "&:hover": {
+                  color: "#18B0E6",
+                },
+                cursor: "pointer",
+                fontSize: "17px",
+              }}
+            >
+              {text2}
+            </Typography>
+          </Link>
+
+          <Link
+            style={{ textDecoration: "none" }}
+            href={{ pathname: href_three }}
+          >
+            <Typography
+              sx={{
+                color: "#153A5F",
+                marginTop: "10px",
+                "&:hover": {
+                  color: "#18B0E6",
+                },
+                cursor: "pointer",
+                fontSize: "17px",
+              }}
+            >
+              {text3}
+            </Typography>
+          </Link>
+
+          <Link
+            style={{ textDecoration: "none" }}
+            href={{ pathname: href_four }}
+          >
+            <Typography
+              sx={{
+                color: "#153A5F",
+                marginTop: "10px",
+                "&:hover": {
+                  color: "#18B0E6",
+                },
+                cursor: "pointer",
+                fontSize: "17px",
+              }}
+            >
+              {text4}
+            </Typography>
+          </Link>
+
+          <Link
+            style={{ textDecoration: "none" }}
+            href={{ pathname: href_five }}
+          >
+            <Typography
+              sx={{
+                color: "#153A5F",
+                marginTop: "10px",
+                "&:hover": {
+                  color: "#18B0E6",
+                },
+                cursor: "pointer",
+                fontSize: "17px",
+              }}
+            >
+              {text5}
+            </Typography>
+          </Link>
+        </CardContent>
+      </Card>
+    </Box>
+  );
 }
 
 export default FooterCard;
