b788190b7ecfe28bf77c63b81fb7cee1dc8cd082
UsmanZulfiqar <apple@apples-MacBook-Pro.local>
2024-02-20 18:32:11 +0500

    Adding Animmation to CoreCard

--- components/About/CoreCard.js
+++ components/About/CoreCard.js
@@ -1,90 +1,86 @@
-import React from "react";
+import React, { useState } from "react";
 import { Box, Typography, Card, CardContent } from "@mui/material";
 
-function CoreCard({image,heading,bg,para}) {
-  const [isHovered, setIsHovered] = React.useState(false);
+function CoreCard({ image, heading, bg, para }) {
+  const [isHovered, setIsHovered] = useState(false);
+
   return (
-    <Box
-      sx={{
-        marginTop: "2%",
-      }}
-    >
+    <Box sx={{ marginTop: "2%" }}>
       <Card
         sx={{
           width: { xs: "21rem", sm: "20rem", md: "22rem", lg: "21rem" },
           height: { xs: "22rem", sm: "22rem", md: "20rem", lg: "19rem" },
           background: bg,
           borderRadius: "15px",
-          cursor:'pointer',
-          boxShadow:"none"
+          cursor: "pointer",
+          boxShadow: "none",
+          position: "relative",
         }}
+        onMouseEnter={() => setIsHovered(true)}
+        onMouseLeave={() => setIsHovered(false)}
       >
-        {!isHovered && (
-          <CardContent
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginTop: "7%",
+            marginBottom: "5%",
+            position: "relative",
+            transition: "transform 0.5s ease",
+            zIndex: 1,
+          }}
+        >
+          <img
+            src={image}
+            alt=""
+            style={{ width: "35%", filter: "invert(100%)" }}
+          />
+          <Typography
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: "7%",
-              marginBottom: "5%",
-              animation: isHovered
-              ? "slideUp 0.5s forwards"
-              : "slideDown 0.5s forwards",
-              
+              color: "white",
+              fontWeight: "600",
+              fontSize: "2.1em",
+              marginTop: "10%",
             }}
-            onMouseEnter={() => setIsHovered(true)}
           >
-            <img
-              src={image}
-              alt=""
-              style={{ width: "35%", filter: "invert(100%)" }}
-            />
-            <Typography
-              sx={{
-                color: "white",
-                fontWeight: "600",
-                fontSize: "2.1em",
-                marginTop: "10%",
-              }}
-            >
-              {heading}
-            </Typography>
-          </CardContent>
-        )}
+            {heading}
+          </Typography>
+        </CardContent>
 
-        {isHovered && (
-          <CardContent
+        <CardContent
+          sx={{
+            display: "flex",
+            flexDirection: "column",
+            justifyContent: "center",
+            alignItems: "center",
+            marginBottom: "5%",
+            textAlign: "center",
+            position: "absolute",
+            top: "0",
+            left: "0",
+            width: "100%",
+            height: "100%",
+            background: bg,
+            transition: "transform 0.8s ease",
+            transform: isHovered ? "translateY(0)" : "translateY(100%)",
+            zIndex: 2,
+          }}
+        >
+          <Typography
+            color="white"
             sx={{
-              display: "flex",
-              flexDirection: "column",
-              justifyContent: "center",
-              alignItems: "center",
-              marginTop: "14%",
-              marginBottom: "5%",
-              textAlign: "center",
-              transition: 'transform 0.5s ease',
-             
+              fontSize: "27px",
+              fontWeight: "600",
             }}
-            onMouseLeave={() => setIsHovered(false)}
           >
-            <Typography
-              color="white"
-              sx={{
-                fontSize: "27px",
-                fontWeight: "600",
-              }}
-            >
-              {heading}
-            </Typography>
-            <Typography
-              color="white"
-              sx={{ marginTop: "6%", fontSize: "16px" }}
-            >
-             {para}
-            </Typography>
-          </CardContent>
-        )}
+            {heading}
+          </Typography>
+          <Typography color="white" sx={{ marginTop: "6%", fontSize: "16px" }}>
+            {para}
+          </Typography>
+        </CardContent>
       </Card>
     </Box>
   );
