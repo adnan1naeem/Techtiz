708d0c4b9b3b69c416b0cb6216ea82330ab3eb16
erummehnaz <aali@techtiz.co>
2024-04-05 11:35:46 +0500

    Screen Fixes

--- components/Website/Stacks.js
+++ components/Website/Stacks.js
@@ -77,12 +77,11 @@ function Stack() {
         <Box sx={{ display: "flex", flexWrap: "wrap", gap: "10%", justifyContent: 'center' }}>
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/js-logo.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/reactjs.svg" />
-          <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/appstore.svg" />
+
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/dotnet.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/angular.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/java.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/ruby-white.svg" />
-          <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/react-white-covered.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/nodejs-white.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/jango.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/swift.svg" />
@@ -93,7 +92,7 @@ function Stack() {
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/diamond.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/python.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/bindhi.svg" />
-          <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/android.svg" />
+
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/golang.svg" />
           <WebImage image="https://techtiz.co/wp-content/uploads/2023/09/database.svg" />
         </Box>
